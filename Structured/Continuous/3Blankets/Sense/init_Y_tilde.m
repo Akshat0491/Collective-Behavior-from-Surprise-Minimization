6d0b0d940813587ec0309a25b5f6a5084d06560f
Akshat0491 <ms23001@iisermohali.ac.in>
2025-06-30 14:10:53 +0530

    upatdedt sense folder

--- Structured/Continuous/3Blankets/Sense/init_Y_tilde.m
+++ Structured/Continuous/3Blankets/Sense/init_Y_tilde.m
@@ -1,11 +1,10 @@
-function ret=init_Y_tilde(o,N,L,T)
-    Y_tilde=cell(o,1);
-    for p=1:o
-        Y_tilde_p=cell(N,1);
-        parfor i=1:N
-            Y_tilde_p{i}=zeros(L,T);
+function ret=init_Y_tilde(N,o,L,dl,T)
+    Y_tilde=cell(N,1);
+    parfor i=1:N
+        Y_tilde{i}=cell(o,1);
+        for k=1:o
+            Y_tilde{i}{k}=zeros(L,dl,T)
         end
-        Y_tilde{p}=Y_tilde_p;
     end
     ret=Y_tilde;
 end
---
+++ Structured/Continuous/3Blankets/Sense/update_Y_tilde.m
@@ -0,0 +1,24 @@
+function ret=update_Y_tilde(Y_tilde,G,R_tilde,noise_params,t)
+    %Arguments
+    %Y_tilde: Nx1 cell, each cell having ox1 cell, each of which is having LxdlxT
+    N=length(Y_tilde);
+    o=length(Y_tilde{1});
+    L=size(Y_tilde{1}{1},1);
+    dl=size(Y_tilde{1}{1},2);
+    % parfor i=1:N
+    %     for k=1:o
+    %     Y_tilde{i}{k}(:,:,t)=G(R_tilde{k}(:,:,t),i,L,dl);
+    %     end
+    % end
+
+    for k=1:o
+        temp=R_tilde{k}(:,:,t);
+        parfor i=1:N
+            Y_tilde{i}{k}(:,:,t)=G(temp,i,L,dl)
+        end
+    end
+
+    ret=Y_tilde;
+end
+
+% G=@(R_tilde,i,L) sqrt(sum((A(:,1:L)-A(i,1:L)*0).^2, 2));  % returns a column vector of size Lx1, 
